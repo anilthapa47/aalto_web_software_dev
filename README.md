d3a625287e39f1b32d239876957c4bea43c68cd9
Anil Thapa <anil.thapa@aalto.fi>
2025-08-07 11:01:06 +0300

    csi:1 - get post put temp

--- README.md
+++ README.md
@@ -3,12 +3,23 @@ Main goal of the project :
  
 By the end of the course, you have created a starting point for a discussion forum designed for educational purposes. The forum will allow users to ask, answer, and upvote course-specific questions.
 
+Added chapters:
+1. Server-side development
+2. Client-side development
+3. Client-server interaction
 
 
 ## 🚀 Features
 
 - ✨ Feature One – 
 - ⚡ Feature Two –
+-  Added Feature 
+    1. intro to API and create API that serves servers and processes JSON data
+
+
+
+
+
 - 🔒 Authentication and Role-based Access
 - 🌐 RESTful API integration
 - 📦 Modular and Scalable Architecture
@@ -18,3 +29,4 @@ By the end of the course, you have created a starting point for a discussion for
 ## 📸 Screenshots
 
 
+virtaZb123Bn98
--- wsd-walking-skeleton/server/app.js
+++ wsd-walking-skeleton/server/app.js
@@ -1,97 +1,23 @@
-import { Hono } from "@hono/hono";
-import { cors } from "@hono/hono/cors";
-import { logger } from "@hono/hono/logger";
-//import postgres from "postgres";
+import { Hono } from "jsr:@hono/hono@4.6.5";
 
 const app = new Hono();
 
+let temperature = 20;
 
-
-
-app.get("/", (c) => c.json({ message: "Hello world!" }));
-
-/*
-const todos = await sql`SELECT * FROM todos`;
-return c.json(todos);
-*/
-// GET /courses returns a JSON document that looks as follows: '{"courses": [ {"id": 1, "name": "Web Software Development" }, {"id": 2, "name": "Device-Agnostic Design" } ] }'.
-app.get("/courses", async (c) => {
-  return c.json({
-    courses: [
-      { id: 1, name: "Web Software Development" },
-      { id: 2, name: "Device-Agnostic Design" }
-    ]
-  });
-
-  /*
-  GET /courses/:id, where ':id' is a path variable, returns a JSON document that looks as follows: '{"course": {"id": :id, "name": "Course Name" } }', 
-  where ':id' is retrieved from the path. The name is a string (just "Course Name", not e.g. "Web Software Development", while the id is a number).
-  */
-
-
+app.get("/temperature", (c) => c.json({ temperature }));
+app.post("/temperature", (c) => {
+  temperature++;
+  return c.json({ temperature });
 });
-
-/*
-GET /courses/:id, where ':id' is a path variable, 
-returns a JSON document that looks as follows: '{"course": {"id": :id, "name": "Course Name" } }', where ':id' is retrieved from the path. The name is a string (just "Course Name", not e.g. "Web Software Development", while the id is a number).
-*/
-app.get("/courses/:id", async (c) => {
-  const rawId = Number(c.req.param("id"));
-  const id = Number(rawId);
-  return c.json({
-    course: 
-      { id: id, name: "Course Name" }
-  });
+app.delete("/temperature", (c) => {
+  temperature--;
+  return c.json({ temperature });
 });
-
-// POST /courses receives a JSON document that looks as follows: '{"name": "Course Name" }', where 'name' is a string. It returns a JSON document that looks as follows: '{"course": {"id": 3, "name": "Course Name" } }', where 'id' is a number.
-app.post("/courses", async (c) => {
-  const { name } = await c.req.json();
-  return c.json({
-    course: {
-      id: 3,
-      name: name
-    }
-  });
+app.put("/temperature", async (c) => {
+  const temp_put = await c.req.json();
+  temperature = temp_put.temperature;
+  return c.json({ temperature });
 });
 
 
-// GET /courses/:id/topics, where ':id' is a path variable, returns a JSON document that looks as follows: '{"topics": [ { "id": 1, "name": "Topic 1" }, {"id": 2, "name": "Topic 2" } ] }'. Nothing is done with the path variable.
-
-export default app;
-app.get("/courses/:id/topics", async (c) => {
-  return c.json({
-    topics: [
-      { id: 1, name: "Topic 1" },
-      { id: 2, name: "Topic 2" }
-    ]
-  });
-});
-
-//GET /courses/:cId/topics/:tId/posts, where ':cId' and ':tId' are path variables, returns a JSON document that looks as follows: '{"posts": [ {"id": 1, "title": "Post 1" }, {"id": 2, "title": "Post 2" } ] }'. Nothing is done with the path variables.
-
-app.get("/courses/:cId/topics/:tId/posts", async (c) => {
-  return c.json({
-    posts: [
-      { id: 1, title: "Post 1" },
-      { id: 2, title: "Post 2" }
-    ]
-  });
-});
-
-// GET /courses/:cId/topics/:tId/posts/:pId, where ':cId', ':tId', and ':pId' are path variables, returns a JSON document that looks as follows: '{"post": {"id": :pId, "title": "Post Title" }, "answers": [ { "id": 1, "content": "Answer 1" }, {"id": 2, "content": "Answer 2" } ] }'. The path variables :cId and :tId are not used.
-
-app.get("/courses/:cId/topics/:tId/posts/:pId", async (c) => {
-  const rawpId = c.req.param("pId");
-  const pId = Number(rawpId);
-  return c.json({
-    post: {
-      id: pId,
-      title: "Post Title"
-    },
-    answers: [
-      { id: 1, content: "Answer 1" },
-      { id: 2, content: "Answer 2" }
-    ]
-  });
-});
+Deno.serve({ port: 3000 }, app.fetch);
