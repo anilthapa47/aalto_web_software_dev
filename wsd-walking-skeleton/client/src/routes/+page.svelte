8453c7c9dfb898cb5eadc0402ae0cb69651dd98b
Anil Thapa <anil.thapa@aalto.fi>
2025-08-24 18:32:28 +0300

    formAndAPI

--- wsd-walking-skeleton/client/src/routes/+page.svelte
+++ wsd-walking-skeleton/client/src/routes/+page.svelte
@@ -10,5 +10,5 @@
 
 <div class="flex flex-col items-center mt-10">
   <h1 class="text-3xl font-bold text-gray-800">Welcome!</h1>
-  <a href="/courses" class="mt-4 text-blue-500 hover:underline">Courses</a>
+
 </div>
